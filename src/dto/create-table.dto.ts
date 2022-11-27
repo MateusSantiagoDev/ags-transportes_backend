65d63fe1151f3951cd3927ad89f08e6f54d046ed
Mateus Santiago <mateus_santiago2.3@outlook.com>
2022-11-27 17:04:11 -0300

    feat: implemented method "CreateDto" & "Entity"

---
+++ src/dto/create-table.dto.ts
@@ -0,0 +1,13 @@
+export class createDto {
+  origem: string;
+  destino: string;
+  data: number;
+  hora: number;
+  nome: string;
+  empresa: string;
+  contato: number;
+  email: string;
+  passageiros: number;
+  veiculo: string;
+  informacoes: string;
+}
---
+++ src/entities/table-entity.ts
@@ -0,0 +1,14 @@
+export class Entity {
+  id: string;
+  origem: string;
+  destino: string;
+  data: number;
+  hora: number;
+  nome: string;
+  empresa: string;
+  contato: number;
+  email: string;
+  passageiros: number;
+  veiculo: string;
+  informacoes: string;
+}
